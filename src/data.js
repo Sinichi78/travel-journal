8210316ca9b2073272d99373cd586a9111a6cde0
Paul Stephen Bayot <110324874+Sinichi78@users.noreply.github.com>
2024-08-09 02:11:49 +0800

    modified the data.js

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export default [
     {
         title: "Sydney Opera House",
         location: "AUSTRALIA",
-        googleMapsUrl: "https://source.unsplash.com/JmuyB_LibRo",
+        googleMapsUrl:  "https://goo.gl/maps/681n4jdijgdt3uGS6",
         startDate: "27 May, 2021",
         endDate: "8 Jan, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
@@ -20,7 +20,7 @@ export default [
     {
         title: "Geirangerfjord",
         location: "NORWAY",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://goo.gl/maps/5J6KAPeSgM8CHQwB7",
         startDate: "01 Oct, 2021",
         endDate: "18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
